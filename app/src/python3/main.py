a67d5fc526d6773b8e1deef4bb3127bd8638d425
Diablo73 <ashul2173@gmail.com>
2022-07-12 23:05:07 +0530

    shutdown handling

--- app/src/python3/main.py
+++ app/src/python3/main.py
@@ -1,8 +1,12 @@
 import os
 import flask
-import waitress
+# import waitress
+import logging
 import telegramBot, utils
 
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
 app = flask.Flask("Flask4Telegram")
 
 @app.route("/")
@@ -13,9 +17,25 @@ def start():
 
 @app.route("/refresh/<password>")
 def refresh(password):
-	if password == os.getenv("REFRESH_PASSWORD"):
+	if password == os.getenv("MASTER_PASSWORD"):
 		utils.printLog("The password is CORRECT!!! ✅")
 		return telegramBot.startTelegramBot()
+	else:
+		result = "The password is WRONG!!! ❌"
+		utils.printLog(result)
+		return result
+
+@app.route("/shutdown/<password>")
+def shutdown(password):
+	if password == os.getenv("MASTER_PASSWORD"):
+		utils.printLog("The password is CORRECT!!! ✅")
+		shutdown_func = flask.request.environ.get("werkzeug.server.shutdown")
+		result = "Not running werkzeug!!! ❌"
+		if shutdown_func is not None:
+			result = "Flask werkzeug server is SHUTTING DOWN!!! ✅"
+			shutdown_func()
+		utils.printLog(result)
+		return result
 	else:
 		result = "The password is WRONG!!! ❌"
 		utils.printLog(result)
@@ -25,8 +45,8 @@ def refresh(password):
 def main():
 	try:
 		utils.printLog("Initiating flask server!!! ⚠")
-		waitress.serve(app, host="0.0.0.0", port=8080)
-#		app.run(host='0.0.0.0', port=8080)
+#       waitress.serve(app, host="0.0.0.0", port=8080)
+		app.run(host='0.0.0.0', threaded=True, port=8080)
 		utils.printLog("Flask server is live and RUNNING!!! ✅")
 	except Exception as e:
 		utils.printLog(e)
