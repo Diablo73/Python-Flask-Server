a5cffd1d655998f9d75c734bf9f7edaf1e8979ec
Diablo73 <ashul2173@gmail.com>
2022-08-01 02:17:33 +0530

    wrap json

--- app/src/python3/controllers/jsonControllers.py
+++ app/src/python3/controllers/jsonControllers.py
@@ -4,13 +4,13 @@ import json
 # @app.route("/json/minify/<jsonArg>", methods=['GET'])
 def jsonMinify(jsonArg):
 	try:
-		return "<pre>" + json.dumps(json.loads(jsonArg), separators=(',', ':')) + "</pre>"
+		return "<pre style='white-space: pre-wrap;'>" + json.dumps(json.loads(jsonArg), separators=(',', ':')) + "</pre>"
 	except Exception as e:
 		return jsonArg + "\n\n❌" + str(e)
 
 # @app.route("/json/beautify/<jsonArg>", methods=['GET'])
 def jsonBeautify(jsonArg):
 	try:
-		return "<pre>" + json.dumps(json.loads(jsonArg), indent=4).replace("    ", "&#9;") + "</pre>"
+		return "<pre style='white-space: pre-wrap;'>" + json.dumps(json.loads(jsonArg), indent=4).replace("    ", "&#9;") + "</pre>"
 	except Exception as e:
 		return jsonArg + "\n\n❌" + str(e)
